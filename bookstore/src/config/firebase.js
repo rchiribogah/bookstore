6412552fca097f8c55b9eccfe4d8e6f910d1fb01
rchiribogah@gmail.com <rchiribogah@gmail.com>
2018-04-21 16:33:02 -0500

    webpack

---
+++ bookstore/src/config/firebase.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyB3n8E7usCDG3Cr-Jj8c4-ihhHGGUBjxF0",
+    authDomain: "bookstore-1851c.firebaseapp.com",
+    databaseURL: "https://bookstore-1851c.firebaseio.com",
+    projectId: "bookstore-1851c",
+    storageBucket: "bookstore-1851c.appspot.com",
+    messagingSenderId: "322017331936"
+  };
---
+++ bookstore/src/firebase.js
@@ -0,0 +1,13 @@
+<script src="https://www.gstatic.com/firebasejs/4.13.0/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB3n8E7usCDG3Cr-Jj8c4-ihhHGGUBjxF0",
+    authDomain: "bookstore-1851c.firebaseapp.com",
+    databaseURL: "https://bookstore-1851c.firebaseio.com",
+    projectId: "bookstore-1851c",
+    storageBucket: "bookstore-1851c.appspot.com",
+    messagingSenderId: "322017331936"
+  };
+  firebase.initializeApp(config);
+</script>
